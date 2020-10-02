156a56ac29160a6140abb96f8a8df668de0a1ba7
YEUNGMelisande <71616487+YEUNGMelisande@users.noreply.github.com>
2020-10-02 14:52:08 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,18 @@
+let Compass = 0
+input.onButtonPressed(Button.A, function () {
+    Compass = input.compassHeading()
+    if (true) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else if (false) {
+    	
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+	
+})
